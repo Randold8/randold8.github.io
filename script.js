0a68c95df05fce79122d60a3441ab42763f72722
Randold8 <84187343+Randold8@users.noreply.github.com>
2021-07-03 22:24:28 +0300

    update!!!
    super nice

--- script.js
+++
@@ -1,6 +0,0 @@
-let page = document.querySelector('.page');
-let themeButton = document.querySelector('.theme-button');
-let content = document.querySelector('.about-content');
-themeButton.onclick = function() {
-	page.classList.toggle('dark-theme');
-}
