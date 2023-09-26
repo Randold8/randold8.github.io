f75868268a611d54ce2fa942e82d9da99c4228d6
Randold8 <84187343+Randold8@users.noreply.github.com>
2023-09-27 02:20:11 +0300

    Add files via upload

---
+++ js/dom1.js
@@ -0,0 +1,32 @@
+for (let i = 5; i <= 8; i++) { 
+  if (i === 7) {
+      continue; 
+    }
+  const value = document.getElementsByClassName('block' + i)[0].innerText;
+  console.log(value);
+}
+
+function updateValues() {
+
+  const surnameEl = document.querySelector('.block4');
+  const nameEl = document.querySelector('.block5'); 
+  const birthYearEl = document.querySelector('.block8');
+
+  const randomSurname = generateRandomString(8);
+  const randomName = generateRandomString(6);
+  const randomBirthYear = generateRandomString(4);
+
+  surnameEl.innerText = randomSurname;
+  nameEl.innerText = randomName;
+  birthYearEl.innerText = randomBirthYear;
+
+}
+
+function generateRandomString(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
